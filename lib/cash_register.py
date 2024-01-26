397bd4dc979737980e49581db2692d446fb88db6
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-26 11:24:24 +0300

    completed lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
-
+ 
 class CashRegister:
-  pass
+    
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.last_transactions = []
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([item] * quantity)
+        self.last_transactions.append({'item': item, 'price': price, 'quantity': quantity})
+
+    def apply_discount(self):
+        if self.discount > 0:
+          self.total -= int(self.total * (self.discount / 100))
+          print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
+
+
+    def void_last_transaction(self):
+      if not self.last_transactions:
+        return "There is no transaction to void."
+      amount_to_subtract = self.last_transactions[-1]['price'] * self.last_transactions[-1]['quantity']
+      self.total -= amount_to_subtract
+      self.last_transactions.clear()
+      
+  
+
+cash_register = CashRegister(10)
+cash_register.add_item('milk', 2.50)
+cash_register.add_item('milk', 2.50)
+cash_register.add_item('salt', 2.30)
+cash_register.add_item('scones', 5.45)
+
+print(cash_register.last_transactions)
+cash_register.void_last_transaction()
+print(cash_register.last_transactions)
